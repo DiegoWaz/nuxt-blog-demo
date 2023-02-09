28b829487cad888963a741b1f08d12bbfe94382f
Monsieur Diego Waziri <diego.waziri@mylittle.fr@MLP-diego.waziri@mylittle.fr>
2023-02-09 14:02:12 +0100

    fix - correction image

--- components/Lead.vue
+++ components/Lead.vue
@@ -32,7 +32,7 @@
                   <nuxt-img
                     class="w-8 h-8 rounded-full mr-4 avatar lazy"
                     data-tippy-content="Author Name"
-                    src="http://i.pravatar.cc/300"
+                    src="https://i.pravatar.cc/300"
                     alt="Avatar of Author"
                   />
                   <p class="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
